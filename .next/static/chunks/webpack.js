5df1dc38c58214a7c8b2a4ac9f80a1d00cc90f25
praweb <prasanna.gaddam@shiftcommerce.com>
2021-07-01 23:36:00 +0100

    disable fallback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ea009a534a4f61411170"; }
+/******/ 		__webpack_require__.h = function() { return "2a1c160a1cdc7dd064ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -50,6 +50,6 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug) => ({params: {slug}})),
-    fallback: true,
+    fallback: false,
   }
 }
